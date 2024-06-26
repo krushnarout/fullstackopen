d77d93a0ce92809aa1cdd4df770144ed33abcdb3
krushnarout <krushnakantarout90@gmail.com>
2024-06-26 18:27:52 +0530

    Add solution of courseinfo exercise 2.5

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,39 +1,5 @@
-const Header = ({ course }) => {
-  return <h2>{course}</h2>
-}
-
-const Part = ({ part }) => {
-  return (
-    <p>
-      {part.name} {part.exercises}
-    </p>
-  )
-}
-
-const Content = ({ course }) => {
-  const total = course.parts.reduce((s, p) => s + p.exercises, 0)
-  console.log(total)
-
-  return (
-    <div>
-      {course.parts.map((part) => (
-        <Part key={part.id} part={part} />
-      ))}
-      <p>
-        <strong>total of {total} exercises</strong>
-      </p>
-    </div>
-  )
-}
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content course={course} />
-    </div>
-  )
-}
+import React from 'react'
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
