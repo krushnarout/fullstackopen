b53dfc1a6e4d04df727fe72bfadfa3b8abda42e4
krushnarout <krushnakantarout90@gmail.com>
2024-06-26 13:50:58 +0530

    Add solution of courseinfo exercise 2.2 & 2.3

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -11,11 +11,17 @@ const Part = ({ part }) => {
 }
 
 const Content = ({ course }) => {
+  const total = course.parts.reduce((s, p) => s + p.exercises, 0)
+  console.log(total)
+
   return (
     <div>
       {course.parts.map((part) => (
         <Part key={part.id} part={part} />
       ))}
+      <p>
+        <strong>total of {total} exercises</strong>
+      </p>
     </div>
   )
 }
