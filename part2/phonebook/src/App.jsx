e11ab4e0321ddfd5546cca6231ab192856071c67
krushnarout <krushnakantarout90@gmail.com>
2024-06-28 15:56:53 +0530

    Add solution of phonebook exercise 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from "react"
 const App = (props) => {
   const [persons, setPersons] = useState(props.persons)
   const [newName, setNewName] = useState("")
+  const [newNumber, setNewNumber] = useState("")
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -11,19 +12,26 @@ const App = (props) => {
       alert(`${newName} is already added to the phonebook`)
       return
     }
-    
+
     const newPerson = {
       id: persons.length + 1,
       name: newName,
+      number: newNumber,
     }
 
     setPersons(persons.concat(newPerson))
     setNewName("")
+    setNewNumber("")
   }
 
   const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
   }
 
   return (
@@ -34,12 +42,15 @@ const App = (props) => {
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       {persons.map((person) => (
-        <div key={person.name}>{person.name}</div>
+        <div key={person.name}>{person.name} {person.number}</div>
       ))}
     </div>
   )
--- part2/phonebook/src/main.jsx
+++ part2/phonebook/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
 
-const persons = [{ name: "Arto Hellas" }]
+const persons = [{ name: "Arto Hellas", number: "346-2223643" }]
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
