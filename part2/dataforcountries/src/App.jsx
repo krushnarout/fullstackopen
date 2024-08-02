7c0606c19eafb7da72f60b8db52dfd6e80948c67
krushnarout <krushnakantarout90@gmail.com>
2024-08-02 15:13:39 +0530

    Add solution of data for countries exercise 2.20

--- part2/dataforcountries/src/App.jsx
+++ part2/dataforcountries/src/App.jsx
@@ -6,6 +6,7 @@ const App = () => {
   const [countries, setCountries] = useState([])
   const [filter, setFilter] = useState("")
   const [selectedCountry, setSelectedCountry] = useState(null)
+  const [weather, setWeather] = useState(null)
 
   useEffect(() => {
     axios
@@ -15,14 +16,35 @@ const App = () => {
       })
   }, [])
 
+  useEffect(() => {
+    if (selectedCountry) {
+      const apiKey = import.meta.env.VITE_OPENWEATHER_API_KEY
+      const capital = selectedCountry.capital[0]
+      axios
+        .get(
+          `https://api.openweathermap.org/data/2.5/weather?q=${capital}&appid=${apiKey}&units=metric`
+        )
+        .then((response) => {
+          setWeather(response.data)
+        })
+    }
+  }, [selectedCountry])
+
   const handleFilterChange = (event) => {
     setFilter(event.target.value)
     setSelectedCountry(null)
+    setWeather(null)
   }
 
   const handleShowClick = (country) => {
     setSelectedCountry(country)
   }
+
+  useEffect(() => {
+    if (countriesToShow.length === 1) {
+      setSelectedCountry(countriesToShow[0])
+    }
+  }, [filter, countries])
 
   const countriesToShow = countries.filter((country) =>
     country.name.common.toLowerCase().includes(filter.toLocaleLowerCase())
@@ -37,7 +59,20 @@ const App = () => {
         {countriesToShow.length > 10 ? (
           <p>Too many matches, specify another filter</p>
         ) : selectedCountry ? (
-          <Country country={selectedCountry} />
+          <div>
+            <Country country={selectedCountry} />
+            {weather && (
+              <div>
+                <h2>Weather in {selectedCountry.capital[0]}</h2>
+                <p>Temperature {weather.main.temp} Celcius</p>
+                <img
+                  src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+                  alt="weather icon"
+                />
+                <p>wind {weather.wind.speed} m/s</p>
+              </div>
+            )}
+          </div>
         ) : countriesToShow.length === 1 ? (
           <Country country={countriesToShow[0]} />
         ) : (
