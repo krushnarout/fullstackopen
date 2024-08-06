d0a38990a5249b2dd014d94e6a32666d728fa29f
krushnarout <krushnakantarout90@gmail.com>
2024-08-06 14:45:38 +0530

    Add solution of phonebook exercise 3.6

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -50,6 +50,16 @@ app.get('/api/persons/:id', (request, response) => {
 app.post('/api/persons', (request, response) => {
   const body = request.body
 
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: 'name or number missing'
+    });
+  } else if (persons.find(person => person.name === body.name)) {
+    return response.status(400).json({
+      error: 'name must be unique'
+    });
+  }
+
   const person = {
     id: Math.floor(Math.random() * 1000).toString(),
     name: body.name,
