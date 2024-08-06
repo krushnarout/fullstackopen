fbd0edc7a68d98183a326ede4c8495d312918094
krushnarout <krushnakantarout90@gmail.com>
2024-08-06 14:10:28 +0530

    Add solution of phonebook exercise 3.5

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3001
+app.use(express.json())
 
 let persons = [
   {
@@ -44,6 +45,19 @@ app.get('/api/persons/:id', (request, response) => {
     console.log('x')
     response.status(404).end()
   }
+})
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  const person = {
+    id: Math.floor(Math.random() * 1000).toString(),
+    name: body.name,
+    number: body.number
+  }
+
+  persons = persons.concat(person)
+  response.json(person)
 })
 
 app.delete('/api/persons/:id', (request, response) => {
