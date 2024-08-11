d0c2948a363af2ae420247bd9c2b036452735395
krushnarout <krushnakantarout90@gmail.com>
2024-08-11 15:12:29 +0530

    Add solution of database exercise 3.12

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -70,11 +70,11 @@ app.post('/api/persons', (request, response) => {
   if (!body.name || !body.number) {
     return response.status(400).json({
       error: 'name or number missing'
-    });
+    })
   } else if (persons.find(person => person.name === body.name)) {
     return response.status(400).json({
       error: 'name must be unique'
-    });
+    })
   }
 
   const person = {
