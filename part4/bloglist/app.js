e98808739dc9377692256851e7154141042bcc1d
krushnarout <krushnakantarout90@gmail.com>
2024-08-19 14:22:07 +0530

    Add solution of blog list exercise 4.2

---
+++ part4/bloglist/app.js
@@ -0,0 +1,40 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
+const morgan = require('morgan')
+
+mongoose.set('strictQuery', false)
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+
+morgan.token('post', function (req) {
+  if (req.method === 'POST') {
+    return JSON.stringify(req.body)
+  } else {
+    return ' '
+  }
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post'))
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.errorHandler)
+
+module.exports = app
