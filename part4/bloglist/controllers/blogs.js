f36ed37fe72035d3f5098923f59c6b737b8a647a
krushnarout <krushnakantarout90@gmail.com>
2024-08-31 15:08:06 +0530

    Add solution of blog list exercise 4.21

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -3,14 +3,6 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
-const getTokenFrom = request => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.startsWith('Bearer ')) {
-    return authorization.replace('Bearer ', '')
-  }
-  return null
-}
-
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog
     .find({}).populate('user', { username: 1, name: 1, id: 1 })
@@ -58,8 +50,21 @@ blogsRouter.post('/', async (request, response, next) => {
 })
 
 blogsRouter.delete('/:id', async (request, response, next) => {
-  await Blog.findByIdAndDelete(request.params.id)
-  response.status(204).end()
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({error: "token missing or invalid"})
+  }
+
+  const id = request.params.id
+  const blog = await Blog.findById(id)
+  if (blog && blog.user.toString() === decodedToken.id) {
+    await Blog.findByIdAndRemove(id)
+    response.status(204).end()
+  }
+  return response.status(401).json({
+    error: "Unauthorized to access the blog",
+  })
 })
 
 blogsRouter.put('/:id', async (request, response, next) => {
