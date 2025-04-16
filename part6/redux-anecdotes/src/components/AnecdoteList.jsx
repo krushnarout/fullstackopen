e1d068b9a949d3d32cbd02b62e6b74eb59265f5e
Krushna Kanta Rout <129386740+krushnarout@users.noreply.github.com>
2025-04-16 15:42:21 +0530

    Add solution of anecdotes exercise 6.9

--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -2,8 +2,13 @@ import { useSelector, useDispatch } from 'react-redux'
 import { voteAnecdote } from '../reducers/anecdoteReducer'
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector(state => [...state].sort((a, b) => b.votes - a.votes))
   const dispatch = useDispatch()
+
+  const anecdotes = useSelector(({ anecdotes, filter }) => {
+    return anecdotes
+      .filter(a => a.content.toLowerCase().includes(filter.toLowerCase()))
+      .sort((a, b) => b.votes - a.votes)
+  })
 
   const vote = (id) => {
     console.log('vote', id)
